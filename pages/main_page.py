43eb4b056a33e9619093e3436fb5d0234dbae2d0
YuriKey <krenev_yu@mail.ru>
2025-07-22 09:13:33 +0400

    some_fix_1

--- pages/main_page.py
+++ pages/main_page.py
@@ -11,7 +11,7 @@ class MainPage(BasePage):
         super().__init__(browser)
 
     EXPECTED_TEXTS = {
-        'url_title': 'Get Online Selenium Certification Course | Way2Automation',
+        'url_title': 'Get Online Selenium Certification Course | Way2Automationn',
         'reg_button_text': 'Register Now',
         'block_courses_title': 'Best Selenium Certification Course Online',
         'life_time_text': 'Lifetime Membership',
