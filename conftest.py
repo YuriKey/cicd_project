55888789a928f3701c6f78cd684d184dfb31c720
YuriKey <krenev_yu@mail.ru>
2025-07-28 16:45:19 +0400

    some fix docker 16

--- conftest.py
+++ conftest.py
@@ -22,7 +22,11 @@ def pages(browser):
 def browser():
     with allure.step('Запуск браузера'):
         chrome_options = Options()
-        chrome_options.add_argument('--start-maximized')
+        chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--remote-allow-origins=*")
         driver = webdriver.Remote(
             command_executor=selenium_host,
             options=chrome_options
