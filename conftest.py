c32aabe0a559f1d91c278d0944a3cc8b98086875
YuriKey <krenev_yu@mail.ru>
2025-07-30 13:09:54 +0400

    add some for docker 69

--- conftest.py
+++ conftest.py
@@ -24,7 +24,7 @@ def browser():
         options.add_argument("--disable-dev-shm-usage")
 
         options.set_capability("browserName", "chrome")
-        options.set_capability("browserVersion", "128.0")
+        options.set_capability("browserVersion", "114.0")
         options.set_capability("selenoid:options", {
             "enableVNC": False,
             "enableVideo": False,
