3adf5390b3b727d1400deaccd6b1bac1620274ac
YuriKey <krenev_yu@mail.ru>
2025-07-31 07:07:42 +0400

    add some for docker 98

--- conftest.py
+++ conftest.py
@@ -26,15 +26,17 @@ def browser():
         options.add_argument("--disable-dev-shm-usage")
 
         options.set_capability("browserName", "chrome")
-        options.set_capability("browserVersion", "114.0")
+        options.set_capability("browserVersion", "latest")
         options.set_capability("selenoid:options", {
-            "enableVNC": False,
+            "enableVNC": True,
             "enableVideo": False,
+            "enableLog": True
         })
 
         host = "selenoid"
         port = os.getenv("SELENIUM_PORT", "4444")
-        command_executor = f"http://{host}:{port}/wd/hub"  #
+        # command_executor = f"http://{host}:{port}/wd/hub"  #
+        command_executor = os.getenv('SELENOID_URL', 'http://localhost:4444/wd/hub')
 
         driver = webdriver.Remote(
             command_executor=command_executor,
