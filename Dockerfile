d3b04bc71afe70234d276b99a636300e41829722
YuriKey <krenev_yu@mail.ru>
2025-08-03 02:16:38 +0400

    add some for docker 159

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,8 @@
 FROM python:3.9-slim
 
 RUN apt-get update && \
-    apt-get install -y wget unzip gnupg jq && \
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
-    echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list && \
-    apt-get update && \
-    apt-get install -y google-chrome-stable && \
+    apt-get install -y wget unzip gnupg jq curl && \
     rm -rf /var/lib/apt/lists/*
-
-RUN CHROME_VERSION=$(google-chrome --version | awk '{print $3}') && \
-    CHROME_MAJOR_VERSION=$(echo $CHROME_VERSION | cut -d'.' -f1) && \
-    CHROMEDRIVER_VERSION=$(wget -qO- "https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json" | jq -r ".channels.Stable.version") && \
-    wget -O chromedriver.zip "https://storage.googleapis.com/chrome-for-testing-public/$CHROMEDRIVER_VERSION/linux64/chromedriver-linux64.zip" && \
-    unzip chromedriver.zip -d /tmp && \
-    mv /tmp/chromedriver-linux64/chromedriver /usr/bin/ && \
-    chmod +x /usr/bin/chromedriver && \
-    rm -rf chromedriver.zip /tmp/chromedriver-linux64
 
 WORKDIR /app
 COPY . .
--- conftest.py
+++ conftest.py
@@ -18,22 +18,34 @@ def pages(browser):
 def browser():
     with allure.step('Запуск браузера'):
         chrome_options = Options()
+        chrome_options.set_capability("browserName", "chrome")
+        chrome_options.set_capability("browserVersion", "120.0")
         chrome_options.set_capability("selenoid:options", {
             "enableVNC": False,
-            "enableVideo": False
+            "enableVideo": False,
+            "enableLog": True
         })
         chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-setuid-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.binary_location = '/usr/bin/google-chrome'
+        chrome_options.add_argument("--remote-debugging-port=9230")
+        chrome_options.add_argument("--start-maximized")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 
         # driver = webdriver.Chrome(options=chrome_options)
-        driver = Remote(
-            command_executor='http://selenoid:4444/wd/hub',
+        driver = webdriver.Remote(
+            command_executor='http://selenoid:4445/wd/hub',
             options=chrome_options
         )
         driver.implicitly_wait(10)
 
-    yield driver
-    with allure.step('Закрытие браузера'):
+        yield driver
+        with allure.step('Закрытие браузера'):
+            driver.stop_client()
+        driver.close()
         driver.quit()
